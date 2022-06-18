221ef10e750108000dd931cb0de1c9f4cb58dfa3
med amine <aminelife86@gmail.com>
2022-06-19 00:50:00 +0200

    save clodi

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "cloudinary",
+    provider: 'cloudinary',
     providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
+      cloud_name: "deaidwozu",
+      api_key: "311271587814164",
+      api_secret:"o33A9sESVG2wqyo8qvs0CngFXWc",
     },
   },
 });
