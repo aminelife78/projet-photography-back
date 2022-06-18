357599a68631ad4630daeac00535fb506e0d4e4c
med amine <aminelife86@gmail.com>
2022-06-19 01:03:54 +0200

    save clodri

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,19 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: 'cloudinary',
-    providerOptions: {
-      cloud_name: "deaidwozu",
-      api_key: "311271587814164",
-      api_secret:"o33A9sESVG2wqyo8qvs0CngFXWc",
-    },
-  },
-});
+module . exports  =  ( { env } )  =>  ( {
+
+  upload : {
+    config : {
+      provider : 'cloudinary' ,
+      providerOptions : {
+        cloud_name : env ( 'CLOUDINARY_NAME' ) ,
+        api_key : env ( 'CLOUDINARY_KEY' ) ,
+        api_secret : env ( 'CLOUDINARY_SECRET' ) ,
+      } ,
+      actionOptions : {
+        upload : { } ,
+        uploadStream : { } ,
+        delete : { } ,
+      } ,
+    } ,
+  } ,
+  
+} ) ;
