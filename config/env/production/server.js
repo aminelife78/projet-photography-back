87e2b64c5abe5b68fb1f3ad3f84811bd91ec6f0e
med amine <aminelife86@gmail.com>
2022-06-19 14:51:05 +0200

    save comit

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env('MY_HEROKU_URL'),
+  url: env('https://polar-garden-88436.herokuapp.com'),
   app: {
     keys: env.array('APP_KEYS')
   },
